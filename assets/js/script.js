4b51273a4a2b9d2e2d52d69904677fa903d3c7ea
Dustin Smith <dustinsmith1983@gmail.com>
2021-12-05 21:05:27 -0800

    updated styling and added in dynamic github links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
+var repoContainerEl = document.querySelector('#repos-container');
+
 // Navigation grow section
+
 $(function() {
     $("#button1").mouseenter(function() {$("#effect1").addClass("wide")});
     $("#button1").mouseleave(function() {$("#effect1").removeClass("wide")})
@@ -22,6 +25,7 @@ $(function() {
 
 
 // Accordion section
+
 $(function(){
     $("#accordion").accordion({header: "h3" , clearStyle: true ,  heightStyle: "fill", collapsible: true});
     //this will open 1st accordian.
@@ -51,41 +55,50 @@ $(function(){
     });
 });
 
+     
+// GitHub API link creator
 
-// OLD NAV ANIMATION
+var getUserRepos = function (user) {
+        var apiUrl = 'https://api.github.com/users/interceptr83/repos';
+      
+        fetch(apiUrl)
+          .then(function (response) {
+            if (response.ok) {
+              response.json().then(function (data) {
+                displayRepos(data, user);
+              });
+            } else {
+              alert('Error: ' + response.statusText);
+            }
+          })
+          .catch(function (error) {
+            alert('Unable to connect to GitHub');
+          });
+      };
 
-    //$(function() {
-    //  $("#button1").mouseenter(function() {$("#effect1").animate({width: 330}, 700);}) ,
-    //  $("#button1").mouseleave(function() {$("#effect1").animate({width: 225}, 500);})
-    //});
-    //$(function() {
-    //  $("#button2").mouseenter(function() {$("#effect2").animate({width: 330}, 700);}) ,
-    //  $("#button2").mouseleave(function() {$("#effect2").animate({width: 225}, 500);})
-    //});
-    //$(function() {
-    //  $("#button3").mouseenter(function() {$("#effect3").animate({width: 330}, 700);}) ,
-    //  $("#button3").mouseleave(function() {$("#effect3").animate({width: 225}, 500);})
-    //});
-    //$(function() {
-    //  $("#button4").mouseenter(function() {$("#effect4").animate({width: 330}, 700);}) ,
-    //  $("#button4").mouseleave(function() {$("#effect4").animate({width: 225}, 500);})
-    //});
-    //$(function() {
-    //  $("#button5").mouseenter(function() {$("#effect5").animate({width: 330}, 700);}) ,
-    //  $("#button5").mouseleave(function() {$("#effect5").animate({width: 225}, 500);})
-   //});
-    
+      var displayRepos = function (repos, searchTerm) {
+        
+        for (var i = 0; i < repos.length; i++) {
+          var repoName = repos[i].name;
+      
+          var repoEl = document.createElement('div');
+          repoEl.classList = 'list-item flex-row justify-space-between align-center';
+      
+          var titleEl = document.createElement('a');
+          titleEl.classList = "github";
+          titleEl.href = "http://github.com/Interceptr83/" + repoName;
+          titleEl.target = "newtab";
+          titleEl.innerText = repoName;
+      
+          repoEl.appendChild(titleEl);
+      
+          var statusEl = document.createElement('span');
+          statusEl.classList = 'flex-row align-center';
+      
+          repoEl.appendChild(statusEl);
+      
+          repoContainerEl.appendChild(repoEl);
+        }
+      };
 
-
-
-// moving buttons Navigation
-     // $(function() {
-     //   $('#navigation > li').hover(
-     //    function () {
-    //     $('a',$(this)).stop().animate({'marginLeft':'85px'},700);
-     //    },
-     //    function () {
-     //     $('a',$(this)).stop().animate({'marginLeft':'0px'},700);
-     //    }
-    //    );
-     //  });
+      getUserRepos();
